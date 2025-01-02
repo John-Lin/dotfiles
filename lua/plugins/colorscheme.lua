280ba40a343817dd05a11e6d08dd4c5600e37116
johnlin <linton.tw@gmail.com>
2025-01-02 23:27:46 +0800

    change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			-- vim.cmd([[colorscheme base16-tomorrow-night]])
-			vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
+			vim.cmd([[colorscheme base16-tomorrow-night]])
+			-- vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
 		end,
 	},
 }
