372dfbeb7bccd14a977c6e8c505b76b27f0731d2
johnlin <linton.tw@gmail.com>
2025-12-22 23:39:58 +0800

    add ty for type checking

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,81 +1,82 @@
 return {
-	{
-		"folke/neodev.nvim",
-	},
-	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"helm_ls",
-					"lua_ls",
-					"gopls",
-					"basedpyright",
-					"terraformls",
-					"zls",
-					"yamlls",
-					"ruff",
-					"clangd", -- C/C++ language server
-				},
-			})
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim", -- none-ls is an active community fork of null-ls
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-		},
-		lazy = true,
-		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			local formatting = null_ls.builtins.formatting
-			local code_actions = null_ls.builtins.code_actions
-			local diagnostics = null_ls.builtins.diagnostics
+    {
+        "folke/neodev.nvim",
+    },
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "helm_ls",
+                    "lua_ls",
+                    "gopls",
+                    "basedpyright",
+                    "terraformls",
+                    "zls",
+                    "yamlls",
+                    "ruff",
+                    "ty",
+                    "clangd", -- C/C++ language server
+                },
+            })
+        end,
+    },
+    {
+        "nvimtools/none-ls.nvim", -- none-ls is an active community fork of null-ls
+        dependencies = {
+            "nvimtools/none-ls-extras.nvim",
+        },
+        lazy = true,
+        event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+        config = function()
+            local null_ls = require("null-ls")
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            local formatting = null_ls.builtins.formatting
+            local code_actions = null_ls.builtins.code_actions
+            local diagnostics = null_ls.builtins.diagnostics
 
 
-			null_ls.setup({
-				sources = {
-					formatting.shfmt,
-					-- code_actions.shellcheck,
-					-- for go
-					formatting.gofmt,
-					formatting.goimports,
-					code_actions.gomodifytags,
-					code_actions.impl,
-					-- for lua
-					formatting.stylua,
-					require("none-ls.formatting.ruff"),
-					-- for C/C++
-					formatting.clang_format.with({
-						extra_args = { "--style=file", "--fallback-style=LLVM" },
-					}),
-				},
-				-- configure format on save
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ async = false })
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
-	{
-		event = "VeryLazy",
-		"neovim/nvim-lspconfig",
-		dependencies = { "williamboman/mason-lspconfig.nvim" },
-		config = function()
-			require("lsp_config")
-		end,
-	},
+            null_ls.setup({
+                sources = {
+                    formatting.shfmt,
+                    -- code_actions.shellcheck,
+                    -- for go
+                    formatting.gofmt,
+                    formatting.goimports,
+                    code_actions.gomodifytags,
+                    code_actions.impl,
+                    -- for lua
+                    formatting.stylua,
+                    require("none-ls.formatting.ruff"),
+                    -- for C/C++
+                    formatting.clang_format.with({
+                        extra_args = { "--style=file", "--fallback-style=LLVM" },
+                    }),
+                },
+                -- configure format on save
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({ async = false })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
+    {
+        event = "VeryLazy",
+        "neovim/nvim-lspconfig",
+        dependencies = { "williamboman/mason-lspconfig.nvim" },
+        config = function()
+            require("lsp_config")
+        end,
+    },
 }
