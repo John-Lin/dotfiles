a06dec73110424d25acefc0a1e0a70278c3d5129
johnlin <linton.tw@gmail.com>
2026-03-08 23:07:13 +0800

    fix: lua format

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -8,8 +8,8 @@ return {
       -- vim.cmd([[colorscheme base16-tomorrow-night]])
       vim.cmd([[colorscheme base16-everforest-dark-hard]])
       -- Allow terminal background transparency to show through
-      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
-      vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", ctermbg = "NONE" })
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE', ctermbg = 'NONE' })
     end,
   },
 }
